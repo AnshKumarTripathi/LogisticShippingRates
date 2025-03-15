712a38eb629710982cfe3a30df6797b0b6b81b71
Ansh Kumar Triapthi <anshtrips07@gmail.com>
2025-03-15 20:35:16 +0530

    Create simple_intrest.py

---
+++ simple_intrest.py
@@ -0,0 +1,20 @@
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Do not use this in production. Sample purpose only.
+   # Author: Upkar Lidder (IBM)
+
+   # Additional Authors:
+
+# Ansh Kumar Tripathi
+
+# Input:
+p=0
+# p, principal amount
+t=0
+# t, time period in years
+r=0
+# r, annual rate of interest
+
+# Output:
+s=p*t*r
+# simple interest = p*t*r
